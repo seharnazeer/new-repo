0fafb0b7cecacab392e6216b26180e60ec4e426a
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2024-10-22 16:00:06 +0500

    chnage address of backend

--- src/utilities/URL.js
+++ src/utilities/URL.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "https://shippinghack.myace.app/";
+// export const BASE_URL = "https://shippinghack.myace.app/";
+export const BASE_URL="http://ec2-54-172-210-121.compute-1.amazonaws.com:8080/"
+// export const BASE_URL="http://localhost:8001/"
